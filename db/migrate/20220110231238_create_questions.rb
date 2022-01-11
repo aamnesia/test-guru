b9743b0b1b3aa83e82972b59bb0bac80e058b74f
aamnesia <lyapunova.anya@mail.ru>
2022-01-11 19:31:39 +0300

    Add foreign_key

--- db/migrate/20220110231238_create_questions.rb
+++ db/migrate/20220110231238_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[7.0]
   def change
     create_table :questions do |t|
       t.text :body, null: false
-      t.references :test
+      t.references :test, foteign_key: true
 
       t.timestamps
     end
