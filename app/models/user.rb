6fd527b34e08ffc191956484b0ffe3a22e7c3420
aamnesia <lyapunova.anya@mail.ru>
2022-01-18 02:14:08 +0300

    Lesson 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  def tests_by_level(test_level)
+    Test
+    .joins('JOIN user_tests ON tests.id = user_tests.test_id')
+    .where(user_tests: {user_id: self.id}, level: test_level)
+  end
 end
