37d0b17593d51a86ba21fac26e1bd7ae16f56b20
aamnesia <lyapunova.anya@mail.ru>
2022-01-19 23:01:34 +0300

    Lesson 5

--- db/migrate/20220119195219_create_tests_users_join_table.rb
+++ db/migrate/20220119195219_create_tests_users_join_table.rb
@@ -1,7 +1,7 @@
-class CreateJoinTableTestsUsers < ActiveRecord::Migration[7.0]
+class CreateTestsUsersJoinTable < ActiveRecord::Migration[7.0]
   def change
     create_join_table :users, :tests do |t|
-      t.index [:user_id, :test_id]
+      t.index [:user_id, :test_id], unique: true
       # t.index [:test_id, :user_id]
     end
   end
