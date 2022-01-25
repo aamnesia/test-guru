ed91c413ba7f81a37b9d8b8d10ce6cec044568c6
aamnesia <lyapunova.anya@mail.ru>
2022-01-25 12:32:59 +0300

    Lesson 8

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,31 +1,37 @@
 class QuestionsController < ApplicationController
-  before_action :set_test, only: %i[index new create]
-  before_action :set_question, only: %i[show destroy]
+  before_action :set_test, only: %i[new create]
+  before_action :set_question, only: %i[edit update show destroy]
 
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+  #rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def new; end
+  def edit; end
+
+  def show; end
+
+  def new
+    @question = @test.questions.new
+  end
 
   def create
     @question = @test.questions.new(question_params)
     if @question.save
-      render :success
+      render @question
     else
       render :new
     end
   end
 
-  def index
-    render plain: @test.questions.inspect
-  end
-
-  def show
-    render plain: @question.body
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
   end
 
   def destroy
     @question.destroy
-    render plain: 'Question was removed'
+    redirect_to test_path(@question.test)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # root to: 'tests#index'
-
   resources :tests do
     resources :questions , shallow: true
   end
