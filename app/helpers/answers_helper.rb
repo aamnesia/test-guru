71ae969f7bc4b35304a802918a84797d31e37501
aamnesia <lyapunova.anya@mail.ru>
2022-01-27 17:40:33 +0300

    Modified new and show views

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,2 +1,7 @@
 module AnswersHelper
+  TITLES = {new: 'Create New', edit: 'Edit', show: nil}.freeze
+
+  def question_header(answer)
+    "#{TITLES[action_name.to_sym]} Answer on '#{answer.question.body}' Question"
+  end
 end
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,7 @@
 module TestsHelper
+  TITLES = {new: 'Create New', edit: 'Edit'}.freeze
+
+  def test_header
+    "#{TITLES[action_name.to_sym]} Test"
+  end
 end
