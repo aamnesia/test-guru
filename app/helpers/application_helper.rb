be04fc557235fe8233496b97b643ef88fceb3e58
aamnesia <lyapunova.anya@mail.ru>
2022-01-30 23:01:44 +0300

    Lesson 10

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
     link_name = repo.blank? ? author : repo
     link_to link_name, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def flash_message(type)
+    content_tag(:p, flash[type], class: "flash #{type}") if flash[type]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id
 
   validates :email, presence: true
+  validates :email, format: URI::MailTo::EMAIL_REGEXP
+  validates :email, uniqueness: true
+
+  has_secure_password
 
   def tests_by_level(test_level)
     tests.level(test_level)
