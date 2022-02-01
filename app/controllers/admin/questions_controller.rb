6b0253d6e9a25cb4e25ad6e64d570d389bc505d9
aamnesia <lyapunova.anya@mail.ru>
2022-02-01 23:05:52 +0300

    Lesson11

---
+++ app/controllers/admin/questions_controller.rb
@@ -0,0 +1,55 @@
+class Admin::QuestionsController < Admin::BaseController
+
+  before_action :set_test, only: %i[new create]
+  before_action :set_question, only: %i[edit update show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def edit; end
+
+  def show; end
+
+  def new
+    @question = @test.questions.new
+  end
+
+  def create
+    @question = @test.questions.new(question_params)
+    if @question.save
+      redirect_to admin_question_path(@question), notice: "Question was successfully created!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to admin_question_path(@question), notice: "Question was successfully updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to admin_test_path(@question.test), notice: "Question was successfully deleted!"
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found', status: 404
+  end
+
+  def set_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+end
---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,57 @@
+class Admin::TestsController < Admin::BaseController
+
+  before_action :set_test, only: %i[edit update show destroy start]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def edit; end
+
+  def new
+    @test = Test.new
+  end
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @questions = @test.questions
+  end
+
+  def create
+    @test = current_user.created_tests.new(test_params)
+    if @test.save
+      redirect_to admin_test_path(@test), notice: "Test was successfully created by #{current_user.first_name} #{current_user.last_name}!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to admin_test_path(@test), notice: "Test was successfully updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to admin_tests_path, notice: "Test was successfully deleted!"
+  end
+
+  private
+
+  def test_params
+    params.require(:tests).permit(:title, :level, :category_id)
+  end
+
+  def rescue_with_test_not_found
+    render plain: 'Test was not found', status: 404
+  end
+
+  def set_test
+    @test = Test.find(params[:id])
+  end
+
+end
