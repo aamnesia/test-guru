d0457e246b15f697afd1ff5d1607da6f7194c7d5
aamnesia <lyapunova.anya@mail.ru>
2022-02-08 13:59:14 +0300

    Beautified views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
-    flash[:notice] = "Hello, #{user.first_name || 'Anonymus'}"
+    flash[:notice] = "Hello, #{user.first_name || 'Anonymus'}!"
     user.admin? ? admin_tests_path : root_path
   end
 
