2eb89f4903f0ac0cbefaa4833750a857424c563a
Anna <92742853+aamnesia@users.noreply.github.com>
2022-02-15 10:25:02 +0300

    Update form_inline.js

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -1,12 +1,4 @@
 document.addEventListener('turbolinks:load', function () {
-  // var controls = document.querySelectorAll('.form-inline-link')
-  //
-  // if (controls.length) {
-  //   for (var i = 0; i < controls.length; i++) {
-  //     controls[i].addEventListener('click', formInlineLinkHandler)
-  //   }
-  // }
-
   $('.form-inline-link').on('click', formInlineLinkHandler)
 
   var errors = document.querySelector('.resource-errors')
@@ -28,9 +20,7 @@ function formInlineHandler(testId) {
   var link = document.querySelector('.form-inline-link[data-test-id="'+ testId +'"]')
   var $testTitle = $('.test-title[data-test-id="'+ testId +'"]')
   var $formInline = $('.form-inline[data-test-id="'+ testId +'"]')
-  // var testTitle = document.querySelector('.test-title[data-test-id="'+ testId +'"]')
-  // var formInline = document.querySelector('.form-inline[data-test-id="'+ testId +'"]')
-
+  
   $testTitle.toggle()
   $formInline.toggle()
 
@@ -39,14 +29,4 @@ function formInlineHandler(testId) {
   } else {
     link.textContent = 'Edit'
   }
-
-  // if (formInline.classList.contains('hide')) {
-  //   testTitle.classList.add('hide')
-  //   formInline.classList.remove('hide')
-  //   link.textContent = 'Cancel'
-  // } else {
-  //   testTitle.classList.remove('hide')
-  //   formInline.classList.add('hide')
-  //   link.textContent = 'Edit'
-  // }
 }
