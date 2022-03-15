c3375a2f3a48a7d157552512865c001a1c097efc
aamnesia <lyapunova.anya@mail.ru>
2022-03-15 10:32:15 +0300

    fixed db

--- db/migrate/20220314214749_add_success_to_test_passages.rb
+++ db/migrate/20220314214749_add_success_to_test_passages.rb
@@ -2,3 +2,4 @@ class AddSuccessToTestPassages < ActiveRecord::Migration[7.0]
   def change
     add_column :test_passages, :success, :boolean, default: false
   end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_14_214749) do
     t.integer "correct_questions", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "success"
+    t.boolean "success", default: false
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
     t.index ["user_id"], name: "index_test_passages_on_user_id"
